b0561fd4461d5fec64e7a008a46375ca9dafeaa7
Friedrich von Never <friedrich@fornever.me>
2025-11-04 01:45:04 +0100

    extract-metadata: initial implementation of the guessed tags

--- scripts/extract-metadata.fsx
+++ scripts/extract-metadata.fsx
@@ -3,10 +3,21 @@
 // SPDX-License-Identifier: BSL-1.0
 
 #r "nuget: MedallionShell, 1.6.2"
+#r "nuget: Fenrir.Git, 1.0.0"
+#r "nuget: TruePath.SystemIo, 1.10.0"
 
 open System
+open System.Collections.Generic
+open System.IO
 open System.Text
+open System.Text.RegularExpressions
+open FSharp.Control
+open Fenrir.Git
+open Fenrir.Git.Metadata
+open JetBrains.Lifetimes
 open Medallion.Shell
+open TruePath
+open TruePath.SystemIo
 
 let upstreamRepoRemote = "https://github.com/tdlib/td.git"
 
@@ -32,6 +43,69 @@ let tags =
         | _ -> failwithf $"Cannot parse git ls-remote output: \"{line}\"."
     )
 
+let guessTags() = Lifetime.UsingAsync(fun lt -> task {
+    printfn "Enumerating commits…"
+    let dotGit = AbsolutePath(__SOURCE_DIRECTORY__) / "../.git" |> LocalPath
+
+    let index = PackIndex(lt, dotGit)
+    let cMakeVersionRegex = Regex(@"project\(TdLib VERSION (.+?) LANGUAGES CXX C\)", RegexOptions.Compiled)
+    let getTdLibVersion(commit: Commit) = task {
+        let commitTree = commit.Body.Tree
+        let! commitTreeBody = Trees.ReadTreeBody(index, dotGit, commitTree)
+        let cMakeLists = commitTreeBody |> Seq.filter(fun x -> x.Name = "CMakeLists.txt") |> Seq.exactlyOne
+        let! cMakeListsContent =
+            let object = Objects.GetRawObjectPath(dotGit, cMakeLists.Hash).ResolveToCurrentDirectory()
+            if object.Exists()
+            then object.ReadAllTextAsync()
+            else task {
+                let! packed = Packing.ReadPackedObject(index, cMakeLists.Hash)
+                use packed = packed
+                use reader = new StreamReader(packed.Stream)
+                return! reader.ReadToEndAsync()
+            }
+
+        let matches = cMakeVersionRegex.Match cMakeListsContent
+        if not matches.Success
+        then failwithf $"Cannot match CMakeLists.txt content with version regex. Content:\n{cMakeListsContent}"
+
+        return matches.Groups[1].Value
+    }
+
+
+    let branch = Refs.ReadRefs dotGit |> Seq.filter(fun x -> x.Name = "refs/remotes/origin/tdlib") |> Seq.exactlyOne
+    let headCommitHash = branch.CommitObjectId
+    let! headCommit = Commits.ReadCommit(index, dotGit, headCommitHash)
+
+    let! allCommits = Commits.TraverseCommits(dotGit, headCommitHash) |> AsyncSeq.ofAsyncEnum |> AsyncSeq.toArrayAsync
+    printfn $"Total commits: {allCommits.Length}."
+
+    printfn "Discovering versions…"
+    let commitsIntroducingVersions = Dictionary<string, Sha1Hash>()
+    let! currentVersion = getTdLibVersion headCommit
+    let mutable lastVersion = currentVersion
+    let mutable lastCommit = headCommitHash
+
+    let visitVersion newVersion =
+        if Some lastVersion <> newVersion then
+            match commitsIntroducingVersions.TryGetValue lastVersion with
+            | true, commit -> failwithf $"Two commits introducing the same version {lastVersion}: {commit} and {lastCommit}."
+            | false, _ -> commitsIntroducingVersions.Add(lastVersion, lastCommit)
+
+    for commit in allCommits do
+        let! version = getTdLibVersion commit
+        visitVersion(Some version)
+
+        lastCommit <- commit.Hash
+        lastVersion <- version
+
+    visitVersion None
+
+    printfn $"Discovered {commitsIntroducingVersions.Count} versions."
+    return commitsIntroducingVersions
+})
+
+let guessedTags = guessTags().Result
+
 [<CLIMutable>]
 type ReleaseMetadata = {
     Tag: string
@@ -39,18 +113,30 @@ type ReleaseMetadata = {
 }
 
 open System.Text.Json
-open System.IO
+
+let metaFromCommits =
+    guessedTags
+    |> Seq.map(fun kvp -> kvp.Key, kvp.Value.ToString())
+    |> Seq.toArray
+
+let metaFromTags = tags
+
+printfn $"Producing result from {metaFromCommits.Length} releases derived from commits and {metaFromTags.Length} releases derived from tags."
 
 let releases =
-    tags
-    |> Seq.map(fun (commit, tag) ->
-        { Tag = tag; Commit = commit }
-    )
+    let dict = Dictionary<string, string>()
+    for tag, commit in metaFromCommits do dict[tag] <- commit
+    for tag, commit in metaFromTags do dict[tag] <- commit
+
+    dict
+    |> Seq.map(fun kvp -> { Tag = kvp.Key; Commit = kvp.Value })
     |> Seq.sortBy(fun r ->
         if not <| r.Tag.StartsWith "v" then failwithf $"Unexpected tag name: {r.Tag}."
         Version.Parse(r.Tag.Substring 1)
     )
     |> Seq.toArray
+
+printfn $"Resulting set: {releases.Length} releases."
 
 let releasesJsonPath = Path.Combine(__SOURCE_DIRECTORY__, "../data/releases.json")
 
